07b1393f516e6897dceb2dcca5b8c65169f1a538
april <panapril77@gmail.com>
2021-04-27 22:17:35 -0400

    route + home page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <div style={{ padding: 80, textAlign: 'center' }}>
+        <Typography variant='h2'>April Takes Notes</Typography>
+        <Typography variant='subtitle1'>A note-taking app</Typography>
+      </div>
+    </div>
+  );
+}
