41f356a13f92cb409fa115b861dd0848f722752a
april <panapril77@gmail.com>
2021-04-28 11:47:42 -0400

    login form ui

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,23 @@
-import { Typography } from '@material-ui/core';
+import { Button, FormGroup, FormLabel, TextField, Typography } from '@material-ui/core';
 import React from 'react';
 
 export default function Login() {
   return (
     <div>
-      <div style={{ padding: 80, textAlign: 'center' }}>
-        <Typography variant='h5'>Login</Typography>
+      <div style={{ padding:100 }}>
+        <form>
+            <FormGroup>
+                <FormLabel>Email</FormLabel>
+                <TextField variant='outlined' />
+            </FormGroup>
+        <br />
+            <FormGroup>
+                <FormLabel>Password</FormLabel>
+                <TextField variant='outlined' />
+            </FormGroup>
+            <br />
+            <Button variant='contained' color='primary' fullWidth>Login</Button>
+        </form>
       </div>
     </div>
   );
